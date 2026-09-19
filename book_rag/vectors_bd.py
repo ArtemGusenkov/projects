c2f2df940219097d1bfebf2c8a9e2fd7a3e122f6
Artem Gusenkov <artemgu@DESKTOP-89KHH5R.localdomain>
2026-09-19 12:03:33 +0300

    add bd creation

---
+++ book_rag/vectors_bd.py
@@ -0,0 +1,79 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from qdrant_client import QdrantClient, models
+
+from fb2_parser import parse_fb2
+
+load_dotenv()
+
+def make_chunks(data, size=1000, overlap=100):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=size, chunk_overlap=overlap,
+        separators=["\n\n", "\n", ". ", " ", ""],
+    )
+    return [
+        {**record, "text": text, "part": part}
+        for record in data
+        for part, text in enumerate(splitter.split_text(record["text"]), 1)
+    ]
+
+def ebedder():
+    embeddings = OpenAIEmbeddings(
+        model="openai/text-embedding-3-small", 
+        api_key=os.environ["OPENROUTER_API_KEY"], 
+        base_url="https://openrouter.ai/api/v1",
+        check_embedding_ctx_length=False,
+        request_timeout=5,
+        max_retries=2)
+    return embeddings
+
+
+def create_q_client():
+    q_client = QdrantClient(url='http://localhost:6333')
+    return q_client
+
+def create_bd(q_client, bd_name, vec_size=1536):
+    if not q_client.collection_exists(bd_name):
+        q_client.create_collection(bd_name, vectors_config=models.VectorParams(
+            size=vec_size, distance=models.Distance.COSINE,
+        ))
+
+def load_vecs(q_client, bd_name, chunks, emb):
+    ids = [i for i in range(len(chunks))]
+    batch_size = 16
+    if q_client.collection_exists(bd_name):
+        for i in range(0, len(chunks), batch_size):
+            batch = [zip(ids[i:i+batch_size], chunks[i:i+batch_size])]
+            vecs = emb.embed_documents([
+                f'{c['chapter']}\n\n{c['text']}' for _,c in batch
+            ])
+            q_client.upsert(
+                collection_name=bd_name,
+                wait=True,
+                points=[
+                    models.PointStruct(
+                        id=id,
+                        vector=vector,
+                        payload={
+                            **chunk,
+                            'chunk_id' : id
+                        },
+                    )
+                    for (id, chunk), vector in enumerate(zip(batch, vecs))
+                ]
+            )
+
+def bd_pipeline():
+    all_books = []
+    for book in os.listdir(r'texts/'):
+        output = parse_fb2('texts/' + book)
+        all_books.extend(output)
+
+    all_chunks = make_chunks(all_books)
+    q_client = create_q_client()
+    create_bd(q_client, 'Sherlock_books')
+    embeddings = ebedder()
+    load_vecs(q_client, 'Sherlock_books', all_chunks, embeddings)
