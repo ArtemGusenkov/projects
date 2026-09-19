0a03fb9fe57f8dc9478e182579056cbf1aa5f941
Artem Gusenkov <artemgu@DESKTOP-89KHH5R.localdomain>
2026-09-19 17:16:49 +0300

    fix vectors loading 2 bd

--- book_rag/vectors_bd.py
+++ book_rag/vectors_bd.py
@@ -1,13 +1,12 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient, models
 
 from fb2_parser import parse_fb2
 
-load_dotenv()
 
 def make_chunks(data, size=1000, overlap=100):
     splitter = RecursiveCharacterTextSplitter(
@@ -20,7 +19,7 @@ def make_chunks(data, size=1000, overlap=100):
         for part, text in enumerate(splitter.split_text(record["text"]), 1)
     ]
 
-def ebedder():
+def embedder():
     embeddings = OpenAIEmbeddings(
         model="openai/text-embedding-3-small", 
         api_key=os.environ["OPENROUTER_API_KEY"], 
@@ -46,7 +45,7 @@ def load_vecs(q_client, bd_name, chunks, emb):
     batch_size = 16
     if q_client.collection_exists(bd_name):
         for i in range(0, len(chunks), batch_size):
-            batch = [zip(ids[i:i+batch_size], chunks[i:i+batch_size])]
+            batch = list(zip(ids[i:i+batch_size], chunks[i:i+batch_size]))
             vecs = emb.embed_documents([
                 f'{c['chapter']}\n\n{c['text']}' for _,c in batch
             ])
@@ -60,13 +59,14 @@ def load_vecs(q_client, bd_name, chunks, emb):
                         payload={
                             **chunk,
                             'chunk_id' : id
-                        },
+                        }
                     )
-                    for (id, chunk), vector in enumerate(zip(batch, vecs))
+                    for (id, chunk), vector in zip(batch, vecs)
                 ]
             )
 
 def bd_pipeline():
+    load_dotenv()
     all_books = []
     for book in os.listdir(r'texts/'):
         output = parse_fb2('texts/' + book)
@@ -75,5 +75,12 @@ def bd_pipeline():
     all_chunks = make_chunks(all_books)
     q_client = create_q_client()
     create_bd(q_client, 'Sherlock_books')
-    embeddings = ebedder()
+    embeddings = embedder()
     load_vecs(q_client, 'Sherlock_books', all_chunks, embeddings)
+    q_client.close()
+
+if __name__ == '__main__':
+    try:
+        bd_pipeline()
+    except Exception as exc:
+        print(f'ошибка: {exc}')
