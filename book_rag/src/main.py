7763fe9add1001953e0c9991a6d03176edebd07d
Artem Gusenkov <gusenkov-artem@mail.ru>
2026-09-22 13:13:05 +0300

    add retriver with bm25 & hybrid search

--- book_rag/src/main.py
+++ book_rag/src/main.py
@@ -3,19 +3,31 @@ import sys
 from fb2_parser import parse_fb2
 
 from vectors_bd import bd_pipeline, embedder, create_q_client
-from db_search import search, answer_question, answ_print
-
+from retrieval import retriever_search
+from answer_forming import answer_question, answ_print
 from dotenv import load_dotenv
+
+BD_NAME='Sherlock_books'
 
 def main():
     load_dotenv()
 
     q_client = create_q_client()
     emb = embedder()
-    question = sys.argv[1]
-    top_k_points = search(q_client, 'Sherlock_books', emb, question)
-    responcse = answer_question(question, top_k_points)
-    answ_print(responcse)
+    args_in = sys.argv
+    if len(args_in) == 2:
+        question = args_in[1]
+        top_k_points = retriever_search(q_client, BD_NAME, emb, question)
+        responcse = answer_question(question, top_k_points)
+        answ_print(responcse)
+    elif len(args_in) == 3:
+        mode = args_in[1]
+        question = args_in[2]
+        top_k_points = retriever_search(q_client, BD_NAME, emb, question, mode=mode)
+        responcse = answer_question(question, top_k_points)
+        answ_print(responcse)
+    else:
+        raise ValueError('неправильные входные данные')
 
 if __name__ == "__main__":
     try:
--- book_rag/src/retrieval.py
+++ book_rag/src/retrieval.py
@@ -7,20 +7,53 @@ def retriever_search(qclient, bd_name, embedder, question, mode="embeddings", k=
     if mode not in ["bm25", "embeddings", "hybrid"]:
         raise ValueError("неправильный метод поиска чанков")
     if mode == 'bm25':
-        vec_question = models.Document
-        qclient.query_points(
-            collection_name=bd_name,
-
+        vec_question = models.Document(
+            text=question, 
+            model = 'qdrant/bm25',
+            options={'language':'russian'}
         )
-
-
-    if mode == 'embeddings':   
+        response = qclient.query_points(
+            collection_name=bd_name,
+            query=vec_question,
+            using='bm25',
+            limit=k,
+            with_payload=True
+        )
+    elif mode == 'embeddings':   
         vec_question = embedder.embed_query(question)
 
         response = qclient.query_points(
             collection_name=bd_name, 
             query=vec_question,
+            using='embs',
             limit=k,
             with_payload=True
         )
+    elif mode == 'hybrid':
+        emb_question = embedder.embed_query(question)
+        bm25_question = models.Document(
+            text=question, 
+            model = 'qdrant/bm25',
+            options={'language':'russian'}
+        )
+        response = qclient.query_points(
+            collection_name=bd_name,
+            prefetch=[
+                models.Prefetch(
+                    query=emb_question,
+                    using='embs',
+                    limit=k
+                ),
+                models.Prefetch(
+                    query=bm25_question,
+                    using='bm25',
+                    limit=k
+                )
+            ],
+            query=models.FusionQuery(fusion=models.Fusion.RRF),
+            limit=k,
+            with_payload=True
+        )
+
+
     return [{**point.payload, "score":point.score} for point in response.points]
